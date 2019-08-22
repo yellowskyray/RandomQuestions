52163aec5ed5b9b99d2138573c3d182bd32b6522
Brian Tan <briantan2828@gmail.com>
2019-08-22 21:39:53 +0800

    Initial Commit

---
+++ RandomQuestions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomQuestions
+//
+//  Created by Brian Tan on 22/08/2019.
+//  Copyright © 2019 Brian Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
